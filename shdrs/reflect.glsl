7ad01529aa4f9c82aa037623894e207e3cbdb56f
Eiyeron <eiyeron@retroactive.me>
2017-09-14 18:10:06 +0200

    Fixed a bit the shader

--- shdrs/reflect.glsl
+++ shdrs/reflect.glsl
@@ -4,7 +4,7 @@ uniform float time = 0;
 uniform vec2 amplitude = vec2(0 / 128., 8/128.);
 uniform vec2 speed = vec2(1., 2.);
 uniform vec2 frequency = vec2(1, 3);
-uniform float sampling_factor = 0.025;
+uniform float sampling_factor = 0.5;
 vec2 distort(vec2 texture_coords)
 {
     vec2 offset = amplitude * sin(texture_coords.y * frequency + time * speed);
@@ -16,10 +16,9 @@ vec4 effect(vec4 loveColor, Image texture, vec2 texture_coords, vec2 screen_coor
 {
     vec2 uv = distort(texture_coords);
     vec3 color = Texel(texture, uv).rgb;
-    vec3 colorDown = Texel(texture, uv + vec2(0,1/240.)).rgb;
-    vec3 colorUp = Texel(texture, uv + vec2(0,-1/240.)).rgb;
+    vec3 colorDown = Texel(texture, uv + vec2(0,1/(128*2.))).rgb;
+    vec3 colorUp = Texel(texture, uv + vec2(0,-1/(128*2.))).rgb;
     color = color * (1 - sampling_factor*2) + colorDown * sampling_factor + colorUp * sampling_factor;
-    color = color *sin(time);
     if (color.r <= 0/255.)
         return vec4(255/255.,241/255.,232/255.,1);
     else if (color.r <= 95/255.)
