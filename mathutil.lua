401c63f53e2c6fb3008b4da512d8f1754adf9bee
Eiyeron <eiyeron@retroactive.me>
2017-10-10 14:17:04 +0200

    More math utils

--- mathutil.lua
+++ mathutil.lua
@@ -2,6 +2,9 @@ return {
     lerp = function ( a,b,v )
         return a + (b-a)*v
     end,
+    ilerp = function(a,b,v)
+        return (v-a)/(b-a)
+    end,
     clamp = function(v, min, max)
         return math.max(math.min(v, max), min)
     end
